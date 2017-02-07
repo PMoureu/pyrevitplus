ca12465c57160974861699c4953ed118490c2625
PMoureu <prmoureu@gmail.com>
2017-02-07 16:48:59 +0100

    patch0.3.0

--- pyRevitPlus.tab/Misc Tools.panel/Open In Excel.pushbutton/script.py
+++ pyRevitPlus.tab/Misc Tools.panel/Open In Excel.pushbutton/script.py
@@ -1,111 +1,88 @@
 """
+OpenInExcel
+Opens Selected schedules in Excel
+TESTED REVIT API: 2015 | 2016 | 2017
 
-Copyright (c) 2016 WeWork | Gui Talarico
-Base script taken from pyRevit Respository.
+Copyright (c) 2014-2016 Gui Talarico @ WeWork
+github.com/gtalarico
 
-pyRevit Notice
-#################################################################
+This script is part of PyRevitPlus: Extensions for PyRevit
+github.com/gtalarico
+
+--------------------------------------------------------
+PyRevit Notice:
 Copyright (c) 2014-2016 Ehsan Iran-Nejad
-Python scripts for Autodesk Revit
+pyRevit: repository at https://github.com/eirannejad/pyRevit
 
-This file is part of pyRevit repository at https://github.com/eirannejad/pyRevit
-
-pyRevit is a free set of scripts for Autodesk Revit: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-See this link for a copy of the GNU General Public License protecting this package.
-https://github.com/eirannejad/pyRevit/blob/master/LICENSE
 """
 
-__doc__ = "Opens Selected Schedules in a Spreadsheet (Excel, LibreOffice)"
+__doc__ = 'Opens Selected schedules in Excel'
+__author__ = '@gtalarico'
+__version__ = '0.3.0'
 __title__ = "Open in\nExcel"
 
-from Autodesk.Revit.DB import ViewSchedule, ViewScheduleExportOptions
-from Autodesk.Revit.DB import ExportColumnHeaders, ExportTextQualifier
-from Autodesk.Revit.DB import BuiltInCategory
-
 import os
+import sys
+import subprocess
+import time
 
-engine = None
-editors_paths = [
-    r"C:\Program Files\Programs\Office 2010\Office14\EXCEL.exe",
-    r"C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE",
-    r"C:\Program Files (x86)\Microsoft Office\Office14\EXCEL.exe",
-    r"C:\Program Files (x86)\Microsoft Office\Office15\EXCEL.EXE",
-    
-    r"C:\Program Files (x86)\LibreOffice 5\program\scalc.exe",
-    r"C:\Program Files (x86)\LibreOffice 4\program\scalc.exe",
-]   # to add another editor, just check if the name can be used as cmd shortcut
+import rpw
+from rpw import doc, uidoc, DB, UI
 
-def export():    
-    uidoc = __revit__.ActiveUIDocument
-    doc = __revit__.ActiveUIDocument.Document    
-    
-    destination = os.path.expandvars('%temp%\\')
-    # destination = os.path.expandvars('%userprofile%\\desktop')        
-    # destination = os.path.dirname(doc.PathName) #+ '\\schedules'
+# Export Settings
+temp_folder = os.path.expandvars('%temp%\\')
+export_options = DB.ViewScheduleExportOptions()
 
-    vseop = ViewScheduleExportOptions()
-    # vseop.ColumnHeaders = ExportColumnHeaders.None
-    # vseop.TextQualifier = ExportTextQualifier.None
-    # vseop.FieldDelimiter = ','
-    # vseop.Title = False
+# Get Saved Excelp Paths
+saved_paths = os.path.join(os.path.dirname(__file__), 'OpenInExcel_UserPaths.txt')
+if not os.path.exists(saved_paths):
+    UI.TaskDialog.Show('OpenInExcel', 'Could not find the File : \n'
+                       'OpenInExcel_UserPaths.txt \nin:\n{}'.format(
+                       os.path.dirname(__file__)))
+    sys.exit()
 
-    selected_ids = uidoc.Selection.GetElementIds()
+with open(saved_paths) as fp:
+    excel_paths = fp.read().split('\n')
 
-    if not selected_ids.Count:
-        '''If nothing is selected, use Active View'''
-        selected_ids=[ doc.ActiveView.Id ]
-        
-    passed = 0
-    for element_id in selected_ids:
-        passed += 1
-        element = doc.GetElement(element_id)
-        if not isinstance(element, ViewSchedule):
-            print('No schedule in Selection. Skipping...')
-            continue
-
-        filename = "".join(x for x in element.ViewName if x not in {'/','*'}) + '.txt'
-        full_filepath = os.path.join(destination, filename)
-        
-        try:
-            step='EXPORTING schedule {} to TEXTFILE'.format(element.ViewName)
-            element.Export(destination, filename, vseop)
-            print('EXPORTED: {0}\n      TO: {1}\n'.format(element.ViewName, filename))
-            
-            step='OPENING with {}'.format(engine)
-            os.system('start {0} \"{1}\"'.format(engine, full_filepath))
-            if passed == selected_ids.Count:
-                __window__.Close()
-            
-        except:
-            passed -= 1
-            print('Sorry, an Error occured in {}'.format(step))
-            print('...Maybe another dialog is opened ?')
-            print('- Filename: {}'.format(filename))
-            print('- Destination: {}'.format(destination))
-            print('- Editor used : {}\n'.format(lpath))
-    
-    print('Export Ended. You can close this dialog.')
-    
-# loop then split the name to adapt the command in func export()
-for lpath in editors_paths:
-    if os.path.exists(lpath):    
-        basepath = os.path.basename(lpath)
-        engine = os.path.splitext(basepath)[0]
-        if engine : 
-            print('Editor Found. Trying to export...\n')
-            export()
-            break
+for excel_path in excel_paths:
+    if os.path.exists(excel_path):
+        break
 else:
-    print('Could not find editor with the following paths:\n'
-    '{}\n'.format('\n'.join(editors_paths)))
-    print('You can edit this script to add another folder :\n'
-    '{}\n'.format(os.path.realpath(__file__)))
-    
+    UI.TaskDialog.Show('OpenInExcel', 'Could not find Excel Path \n'
+                       'Please add your Excel path to OpenInExcel_UserPaths.txt'
+                       'and try again.')
+    os.system('start notepad \"{path}\"'.format(path=saved_paths))
+    sys.exit()
+
+
+selection = rpw.Selection()
+
+# Get any selected View Schedules or Schedule Instances
+selected_schedules = [e for e in selection.elements
+                      if isinstance(e, (DB.ViewSchedule, DB.ScheduleSheetInstance))]
+
+# If no view is selected, check if Active View is a schedule
+if not selected_schedules and isinstance(uidoc.ActiveView, DB.ViewSchedule):
+    selected_schedules = [uidoc.ActiveView]
+elif not selected_schedules:
+    UI.TaskDialog.Show('OpenInExcel', 'Must have a schedule open or selected \
+                       in the Project Browser.')
+
+for schedule in selected_schedules:
+    if isinstance(schedule, DB.ScheduleSheetInstance):
+        schedule = doc.GetElement(schedule.ScheduleId)
+    schedule_name = "".join([x for x in schedule.ViewName if x.isalnum()])
+
+    # Adds random digits to avoid name clash
+    filename = '{}_{}.txt'.format(schedule_name, str(time.time())[-2:])
+
+    schedule.Export(temp_folder, filename, export_options)
+
+    try:
+        full_filepath = os.path.join(temp_folder, filename)
+        os.system('"{editor}" "{path}"'.format(editor=excel_path, path=full_filepath))
+        continue
+    except:
+        print('Sorry, something failed:')
+
+__window__.Close()
